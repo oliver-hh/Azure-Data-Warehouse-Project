079b3a08890b1cf3dbd36127bab8485f0907a7c1
Oliver Brandt <oliver.brandt@mabanaft.com>
2024-03-22 16:06:22 +0100

    feeature: Adjust date key for dimension table

--- python/create_date_dim.py
+++ python/create_date_dim.py
@@ -22,29 +22,28 @@ def get_season(month):
         return 'Winter' 
 
 # Generate a date range with hourly frequency    
-date_range = pd.date_range(start='2021-01-01', end='2022-12-31 23:00:00', freq='H')    
+date_range = pd.date_range(start='2021-01-01', end='2022-12-31 23:00:00', freq='h')    
   
 # Create a DataFrame    
-df = pd.DataFrame(date_range, columns=['datetime'])    
+df = pd.DataFrame(date_range, columns=['date_key'])    
 
 # Create a list of US holidays  
 us_holidays = holidays.US()  
 
 # Add the other columns    
-df['date_key'] = df.index + 1    
-df['hour'] = df['datetime'].dt.hour    
-df['day'] = df['datetime'].dt.day    
-df['day_of_week'] = df['datetime'].dt.weekday    
-df['week'] = df['datetime'].dt.isocalendar().week    
-df['month'] = df['datetime'].dt.month    
-df['quarter'] = df['datetime'].dt.quarter    
-df['year'] = df['datetime'].dt.year    
+df['hour'] = df['date_key'].dt.hour    
+df['day'] = df['date_key'].dt.day    
+df['day_of_week'] = df['date_key'].dt.weekday    
+df['week'] = df['date_key'].dt.isocalendar().week    
+df['month'] = df['date_key'].dt.month    
+df['quarter'] = df['date_key'].dt.quarter    
+df['year'] = df['date_key'].dt.year    
 df['season'] = df['month'].apply(get_season)  
 df['time_of_day'] = df['hour'].apply(get_time_of_day)    
-df['is_weekend_or_holiday'] = df['datetime'].dt.date.apply(lambda x: x in us_holidays)
+df['is_weekend_or_holiday'] = df['date_key'].dt.date.apply(lambda x: x in us_holidays)
   
 # Rearrange the columns    
-df = df[['date_key', 'datetime', 'year', 'month', 'day', 'hour', 'time_of_day', 'quarter', 'week', 'day_of_week', 'season', 'is_weekend_or_holiday']]    
+df = df[['date_key', 'year', 'month', 'day', 'hour', 'time_of_day', 'quarter', 'week', 'day_of_week', 'season', 'is_weekend_or_holiday']]    
   
 # Write to a CSV file    
 df.to_csv('dim_date.csv', index=False)  
