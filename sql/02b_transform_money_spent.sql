1fb250b9aac75739026f50400325e0e03f4b1866
Oliver Brandt <oliver.brandt@mabanaft.com>
2024-03-25 14:01:34 +0100

    feat: Implement use case "money spent"

--- sql/02b_transform_money_spent.sql
+++ sql/02b_transform_money_spent.sql
@@ -1,1 +1,52 @@
---TODO
+-- dim_rider can be reused
+
+-- dim_date_payment (created from payment)
+IF OBJECT_ID('dbo.dim_date_payment') IS NOT NULL
+BEGIN
+  DROP EXTERNAL TABLE [dbo].[dim_date_payment];
+END
+
+CREATE EXTERNAL TABLE dbo.dim_date_payment
+WITH (
+  LOCATION    = 'dim_date_payment',
+  DATA_SOURCE = [udacity-divvy_dlsazdelab_dfs_core_windows_net],
+	FILE_FORMAT = [SynapseDelimitedTextFormat]
+)
+AS
+SELECT DISTINCT
+  date_key = DATEADD(MONTH, DATEDIFF(MONTH, 0, [date]), 0),
+  month = MONTH([date]),
+  quarter = DATEPART(QUARTER, [date]),
+  year = YEAR([date])
+FROM
+  staging_payment
+ORDER BY
+  date_key;
+GO
+
+-- fact_money_spent
+IF OBJECT_ID('dbo.fact_money_spent') IS NOT NULL
+BEGIN
+  DROP EXTERNAL TABLE [dbo].[fact_money_spent];
+END
+
+CREATE EXTERNAL TABLE dbo.fact_money_spent
+WITH (
+  LOCATION    = 'fact_money_spent',
+  DATA_SOURCE = [udacity-divvy_dlsazdelab_dfs_core_windows_net],
+	FILE_FORMAT = [SynapseDelimitedTextFormat]
+)
+AS
+SELECT
+  date_key = DATEADD(MONTH, DATEDIFF(MONTH, 0, date), 0),
+  rider_key = rider_id,
+  amount_dollar = sum(amount)
+FROM staging_payment
+GROUP BY DATEADD(MONTH, DATEDIFF(MONTH, 0, date), 0), rider_id
+ORDER BY DATEADD(MONTH, DATEDIFF(MONTH, 0, date), 0), rider_id;
+GO
+
+
+SELECT * FROM dim_date_payment
+SELECT TOP 100 * FROM fact_money_spent
+
--- sql/03b_queries_money_spent.sql
+++ sql/03b_queries_money_spent.sql
@@ -1,1 +1,37 @@
---TODO
+-- Overall spent per year, quarter, month
+SELECT
+    d.year, d.quarter, d.month,
+    amount_dollar = SUM(f.amount_dollar)
+FROM
+    fact_money_spent f
+INNER JOIN
+    dim_date_payment d on d.date_key = f.date_key
+GROUP BY
+    d.year, d.quarter, d.month
+ORDER BY
+    d.year, d.quarter, d.month;
+
+-- Top riders per month
+;WITH RiderAmounts AS (  
+    SELECT   
+        d.year, d.quarter, d.month, r.rider_key,  
+        amount_dollar = sum(f.amount_dollar),  
+        ROW_NUMBER() OVER(PARTITION BY d.year, d.quarter, d.month ORDER BY sum(f.amount_dollar) DESC) as rn  
+    FROM  
+        fact_money_spent f  
+    INNER JOIN  
+        dim_date_payment d on d.date_key = f.date_key  
+    INNER JOIN  
+        dim_rider r on r.rider_key = f.rider_key  
+    GROUP BY  
+        d.year, d.quarter, d.month, r.rider_key  
+)  
+SELECT   
+    year, quarter, month, rider_key, amount_dollar   
+FROM   
+    RiderAmounts   
+WHERE   
+    rn = 1  
+ORDER BY  
+    year, quarter, month;  
+
