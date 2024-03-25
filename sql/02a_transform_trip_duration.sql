20618e4439a2ff69fbd74e09b92abd1e3af7b3a0
Oliver Brandt <oliver.brandt@mabanaft.com>
2024-03-25 14:01:06 +0100

    feat: Use own date dimension for trips

--- sql/02a_transform_trip_duration.sql
+++ sql/02a_transform_trip_duration.sql
@@ -6,15 +6,59 @@ Use case: Analyze how much time is spent per ride/trip
 - Based on whether the rider is a member or a casual rider
 */
 
--- dim_station
-IF OBJECT_ID('dbo.dim_station') IS NOT NULL
+-- dim_date_trip
+IF OBJECT_ID('dim_date_trip') IS NOT NULL
 BEGIN
-  DROP EXTERNAL TABLE [dbo].[dbo.dim_station];
+  DROP EXTERNAL TABLE [dbo].[dim_date_trip];
+END
+
+CREATE EXTERNAL TABLE dbo.dim_date_trip
+WITH (
+  LOCATION = 'dim_date_trip',
+  DATA_SOURCE = [udacity-divvy_dlsazdelab_dfs_core_windows_net],
+  FILE_FORMAT = [SynapseDelimitedTextFormat]
+)
+AS
+SELECT DISTINCT
+  [date_key] = DATEADD(HOUR, DATEDIFF(HOUR, 0, start_at), 0),
+	[year] = YEAR(start_at),
+	[month] = MONTH(start_at),
+	[day] = DAY(start_at),
+	[hour] = DATEPART(HOUR, start_at),
+	[time_of_day] = CASE
+    WHEN DATEPART(HOUR, start_at) BETWEEN 5 AND 11 THEN 'Morning'
+    WHEN DATEPART(HOUR, start_at) BETWEEN 12 AND 17 THEN 'Afternoon'
+    WHEN DATEPART(HOUR, start_at) BETWEEN 18 AND 20 THEN 'Evening'
+    ELSE 'Night'
+  END,
+	[quarter] = DATEPART(QUARTER, start_at),
+	[week] = DATEPART(WEEK, start_at),
+	[day_of_week] = DATEPART(WEEKDAY, start_at),
+	[season] = CASE
+    WHEN MONTH(start_at) IN (3, 4, 5) THEN 'Spring'
+    WHEN MONTH(start_at) IN (6, 7, 8) THEN 'Summer'
+    WHEN MONTH(start_at) IN (9, 10, 11) THEN 'Autumn'
+    ELSE 'Winter'
+  END,
+	[is_weekend] = CAST(CASE
+    WHEN DATEPART(WEEKDAY, start_at) IN (1, 7) THEN 1
+    ELSE 0
+  END AS BIT)
+FROM
+  staging_trip
+ORDER BY
+  [date_key]
+GO
+
+-- dim_station
+IF OBJECT_ID('dim_station') IS NOT NULL
+BEGIN
+  DROP EXTERNAL TABLE [dbo].[dim_station];
 END
 
 CREATE EXTERNAL TABLE dbo.dim_station
 WITH (
-  LOCATION    = 'dbo.dim_station',
+  LOCATION    = 'dim_station',
   DATA_SOURCE = [udacity-divvy_dlsazdelab_dfs_core_windows_net],
   FILE_FORMAT = [SynapseDelimitedTextFormat]
 )
@@ -29,24 +73,24 @@ GO
 -- dim_rider
 IF OBJECT_ID('dbo.dim_rider') IS NOT NULL
 BEGIN
-  DROP EXTERNAL TABLE [dbo].[dbo.dim_rider];
+  DROP EXTERNAL TABLE [dbo].[dim_rider];
 END
 
 CREATE EXTERNAL TABLE dbo.dim_rider
 WITH (
-  LOCATION    = 'dbo.dim_rider',
+  LOCATION    = 'dim_rider',
   DATA_SOURCE = [udacity-divvy_dlsazdelab_dfs_core_windows_net],
   FILE_FORMAT = [SynapseDelimitedTextFormat]
 )
 AS
 SELECT
   rider_key = rider_id,
-  age_account_start = (SELECT   
-    YEAR(account_start_date) - YEAR(birthday) -   
-    CASE   
-      WHEN MONTH(account_start_date) < MONTH(birthday)   
-          OR (MONTH(account_start_date) = MONTH(birthday) AND DAY(account_start_date) < DAY(birthday))   
-      THEN 1   
+  age_account_start = (SELECT
+    YEAR(account_start_date) - YEAR(birthday) -
+    CASE
+      WHEN MONTH(account_start_date) < MONTH(birthday)
+          OR (MONTH(account_start_date) = MONTH(birthday) AND DAY(account_start_date) < DAY(birthday))
+      THEN 1
       ELSE 0
     END),
   is_member
@@ -58,24 +102,24 @@ GO
 -- dim_rider_age_trip
 IF OBJECT_ID('dbo.dim_rider_age_trip') IS NOT NULL
 BEGIN
-  DROP EXTERNAL TABLE [dbo].[dbo.dim_rider_age_trip];
+  DROP EXTERNAL TABLE [dbo].[dim_rider_age_trip];
 END
 
 CREATE EXTERNAL TABLE dbo.dim_rider_age_trip
 WITH (
-  LOCATION    = 'dbo.dim_rider_age_trip',
+  LOCATION    = 'dim_rider_age_trip',
   DATA_SOURCE = [udacity-divvy_dlsazdelab_dfs_core_windows_net],
   FILE_FORMAT = [SynapseDelimitedTextFormat]
 )
 AS
 SELECT
   trip_key = t.trip_id,
-  age_trip = (SELECT   
-    YEAR(t.start_at) - YEAR(r.birthday) -   
-    CASE   
-      WHEN MONTH(t.start_at) < MONTH(r.birthday)   
-          OR (MONTH(t.start_at) = MONTH(birthday) AND DAY(t.start_at) < DAY(birthday))   
-      THEN 1   
+  age_trip = (SELECT
+    YEAR(t.start_at) - YEAR(r.birthday) -
+    CASE
+      WHEN MONTH(t.start_at) < MONTH(r.birthday)
+          OR (MONTH(t.start_at) = MONTH(birthday) AND DAY(t.start_at) < DAY(birthday))
+      THEN 1
       ELSE 0
     END)FROM
   staging_trip t
@@ -108,7 +152,7 @@ GO
 
 -- Test queries
 SELECT TOP 10 * FROM dbo.fact_trip_duration
-SELECT TOP 10 * FROM dim_date
+SELECT TOP 10 * FROM dim_date_trip
 SELECT TOP 10 * FROM dim_station
 SELECT TOP 10 * FROM dim_rider
 SELECT TOP 10 * FROM dim_rider_age_trip
--- sql/03a_queries_trip_duration.sql
+++ sql/03a_queries_trip_duration.sql
@@ -1,7 +1,7 @@
 -- Use case: Analyze how much time is spent per ride/trip
 
 SELECT TOP 1000
-    d.season, d.day_of_week, d.time_of_day, d.is_weekend_or_holiday,
+    d.season, d.day_of_week, d.time_of_day, d.is_weekend,
     rat.age_trip,
     r.is_member,
     start_station = ss.name,
@@ -10,7 +10,7 @@ SELECT TOP 1000
 FROM
     fact_trip_duration f
 INNER JOIN
-    dim_date d on d.date_key = f.date_key
+    dim_date_trip d on d.date_key = f.date_key
 INNER JOIN
     dim_rider r on r.rider_key = f.rider_key
 INNER JOIN
@@ -29,14 +29,14 @@ SELECT TOP 10 * FROM dim_rider;
 
 
 -- Date and time factors such as day of week and time of day
-SELECT   
+SELECT
     d.day_of_week,   
     d.time_of_day,   
     avg_duration = AVG(f.duration_in_minutes)
 FROM  
     fact_trip_duration f  
 INNER JOIN  
-    dim_date d on d.date_key = f.date_key  
+    dim_date_trip d on d.date_key = f.date_key  
 GROUP BY  
     d.day_of_week,   
     d.time_of_day
@@ -47,20 +47,20 @@ ORDER BY
 SELECT
     d.season,
     d.time_of_day,   
-    d.is_weekend_or_holiday,   
+    d.is_weekend,   
     avg_duration = AVG(f.duration_in_minutes)
 FROM  
     fact_trip_duration f  
 INNER JOIN  
-    dim_date d on d.date_key = f.date_key  
+    dim_date_trip d on d.date_key = f.date_key  
 GROUP BY  
     d.season,
     d.time_of_day,
-    d.is_weekend_or_holiday
+    d.is_weekend
 ORDER BY
     CASE d.season WHEN 'Spring' THEN 1 WHEN 'Summer' THEN 2 WHEN 'Autumn' THEN 3 WHEN 'Winter' THEN 4 ELSE 5 END,
     CASE d.time_of_day WHEN 'Morning' THEN 1 WHEN 'Afternoon' THEN 2 WHEN 'Evening' THEN 3 WHEN 'Night' THEN 4 ELSE 5 END,
-    d.is_weekend_or_holiday;
+    d.is_weekend;
     
 
 -- Which station is the starting and / or ending station?
@@ -77,7 +77,6 @@ INNER JOIN
 GROUP BY  
     ss.name,   
     es.name
-ORDER BY avg_duration DESC;
 
 -- Age of the rider at time of the ride:
 SELECT   
